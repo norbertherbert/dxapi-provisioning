c56064d587368e5bcb7e7cc5523d8e7cef0e062e
norbi <nherbert@nano-tel.com>
2020-11-03 13:37:17 +0100

    list_apiResources.py includes paging

--- list_apiResources.py
+++ list_apiResources.py
@@ -1,24 +1,42 @@
 import json, sys
-from urllib import error
+from urllib import parse, error
 from urllib.request import Request, urlopen
 
-if len(sys.argv) != 2 :
-    print('usage: list_apiResources.py devices|deviceProfiles|connectivityPlans|routingProfiles|baseStations|baseStationProfiles')
-    quit()
+def print_usage():
+    print('usage: python list_apiResources.py <apiResource> [<pageIndex>]')
+    print('<apiResource> = devices|deviceProfiles|connectivityPlans|routingProfiles|baseStations|baseStationProfiles')
+    print('<pageIndex> is an integer. Numbering starts from 1. The page size is always 100.')
 
-apiResource = sys.argv[1]
-
+# Parse the config file
 try:
     config_file_json = open('config.json', 'r')
 except OSError:
     print('cannot open: config.json')
-    quit()
-
+    sys.exit(1)
 config = json.load(config_file_json)
 
-req = Request(url = config['apiBaseUrl'] + '/core/latest/api/' + apiResource)
-req.add_header('Accept', 'application/json; charset=UTF-8')
-req.add_header('Authorization', 'Bearer ' + config['token'])
+# Parse the cli params
+if (len(sys.argv) < 2) or (len(sys.argv) > 3) :
+    print_usage()
+    sys.exit(1)
+apiResource = sys.argv[1]
+queryParams = {};
+if len(sys.argv) == 3 :
+    try:
+        queryParams['pageIndex'] = int(sys.argv[2])
+    except:
+        print('Invalid <pageIndex>')
+        print_usage()
+        sys.exit(1)
+
+req = Request(
+    url = config['apiBaseUrl'] + '/core/latest/api/' + apiResource + '?' + parse.urlencode(queryParams),
+    headers = {
+        'Accept': 'application/json; charset=UTF-8', 
+        'Authorization': 'Bearer ' + config['token']
+    },
+    method = 'GET'
+)
 try:
     body = urlopen(req).read().decode('utf-8')
 except error.HTTPError as e:
